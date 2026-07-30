cda9d51ea1362eb27c909833c419b978e8d09df4
vatsanmol07 <vatsanmol96@gmail.com>
2026-07-30 21:14:26 +0530

    Time: 3 ms (11.73%), Space: 8.3 MB (77.73%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+      int res=0;
+      for(int i=0;i<word.size();i++){
+        res+=(i/8+1);
+      }  
+      return res;
+    }
+};
