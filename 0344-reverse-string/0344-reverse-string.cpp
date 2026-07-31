bcc24167cd745367529a465081ac3a902f678e20
vatsanmol07 <vatsanmol96@gmail.com>
2026-07-31 11:49:34 +0530

    Time: 0 ms (100%), Space: 27.3 MB (53.62%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-       stack<char>st;
-       for(int i=0;i<s.size();i++){
-        st.push(s[i]);
-       } 
-       int i=0;
-       while(st.size()!=0){//while(!st.empty()) bhi use kar sakte hain
-        s[i]=st.top();
-        i++;
-        st.pop();
-       }
+        int left = 0;
+        int right = s.size() - 1;
 
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
     }
 };
