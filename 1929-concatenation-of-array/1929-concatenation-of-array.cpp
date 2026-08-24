fb2693d352493a384d033f85a16808c4c9a5f0db
vatsanmol07 <vatsanmol96@gmail.com>
2026-08-24 18:14:50 +0530

    Time: 0 ms (100%), Space: 17 MB (40.59%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-       vector<int>ans;
-       for(int i=0;i<nums.size();i++){
-        ans.push_back(nums[i]);
-       }
-       for(int i=0;i<nums.size();i++){
-        ans.push_back(nums[i]);
-       }
+       vector<int>ans=nums;
+       ans.insert(ans.end(),nums.begin(),nums.end());
        return ans;
     }
 };
